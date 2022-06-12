d17f8f62be9add9831ba022fc5611adb8c56a776
Dmitriy <dima.komlyk@gmail.com>
2022-06-12 13:11:49 +0300

    Added route users getAll

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,5 +6,15 @@ const { responseMiddleware } = require('../middlewares/response.middleware');
 const router = Router();
 
 // TODO: Implement route controllers for user
+router.get('/', async (req, res, next) => {
+    try {
+        const users = await UserService.getAll();
+        res.data = users;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
 
 module.exports = router;

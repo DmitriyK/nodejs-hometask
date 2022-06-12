a8f7d040716046e45b401844f1bf79443d409a82
Dmitriy <dima.komlyk@gmail.com>
2022-06-12 13:14:49 +0300

    Added getAll method

--- services/userService.js
+++ services/userService.js
@@ -1,8 +1,14 @@
 const { UserRepository } = require('../repositories/userRepository');
 
 class UserService {
-
     // TODO: Implement methods to work with user
+    getAll() {
+        const item = UserRepository.getAll();
+        if(!item) {
+            return null;
+        }
+        return item;
+    }
 
     search(search) {
         const item = UserRepository.getOne(search);

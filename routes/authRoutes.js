38778c9c9b2adc68a07bd2e5c66da0c8e2f5cfa2
Dmitriy <dima.komlyk@gmail.com>
2022-06-12 17:45:03 +0300

    Added login route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,9 +4,14 @@ const { responseMiddleware } = require('../middlewares/response.middleware');
 
 const router = Router();
 
-router.post('/login', (req, res, next) => {
+router.post('/login', async (req, res, next) => {
     try {
         // TODO: Implement login action (get the user if it exist with entered credentials)
+        const data = await AuthService.login({ email: req.body.email });
+        if ( data.password !== req.body.password ){
+            res.status(400);
+            throw Error('Incorrect password');
+        }
         res.data = data;
     } catch (err) {
         res.err = err;

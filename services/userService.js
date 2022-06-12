ef80439928e44ec9b50fa700217c9d0e6daa95af
Dmitriy <dima.komlyk@gmail.com>
2022-06-12 17:26:29 +0300

    Added create method

--- services/userService.js
+++ services/userService.js
@@ -1,22 +1,30 @@
 const { UserRepository } = require('../repositories/userRepository');
 
 class UserService {
-    // TODO: Implement methods to work with user
-    getAll() {
-        const item = UserRepository.getAll();
-        if(!item) {
-            return null;
-        }
-        return item;
+  // TODO: Implement methods to work with user
+  getAll() {
+    const item = UserRepository.getAll();
+    if(!item) {
+      return null;
     }
+    return item;
+  }
 
-    search(search) {
-        const item = UserRepository.getOne(search);
-        if(!item) {
-            return null;
-        }
-        return item;
+  search(search) {
+    const item = UserRepository.getOne(search);
+    if(!item) {
+      return null;
     }
+    return item;
+  }
+
+  create(data) {
+    const item = UserRepository.create(data);
+    if(!item) {
+      return null;
+    }
+    return item;
+  }
 }
 
 module.exports = new UserService();

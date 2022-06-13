376fa6fe5e46470befde1f51be47feedfb42a5bf
Dmitriy <dima.komlyk@gmail.com>
2022-06-13 22:04:56 +0300

    Fixed spaces

--- middlewares/user.validation.middleware.js
+++ middlewares/user.validation.middleware.js
@@ -1,59 +1,48 @@
 const { user } = require('../models/user');
 const ValidationService = require('../services/validationService');
+
 const createUserValid = (req, res, next) => {
-  // TODO: Implement validatior for user entity during creation
-  try {
-    const data = req.body;
-    if(Object.keys(data).length === 0){
-        throw Error('Request body cannot be empty')
+    // TODO: Implement validatior for user entity during creation
+    try {
+        const dataUser = req.body;
+        if (Object.keys(dataUser).length === 0) {
+            throw Error('User entity must not be empty')
+        }
+        for (let key in dataUser) {
+            if (key === 'id' || !user.hasOwnProperty(key)) {
+                throw Error('User entity to create isn\'t valid');
+            }
+        }
+        const userValid = ValidationService.validateUser({ ...user, ...dataUser });
+        if (userValid) {
+            req.body = userValid;
+            next();
+        }
+    } catch (err) {
+        res.status(400).json({ error: true, message: err.message })
     }
-    for (let prop in data) {
-      switch(prop) {
-        case 'firstName':
-          if (!ValidationService.isString(data[prop])) {
-            throw Error('First name is not valid');
-          }
-          break;
-        case 'lastName':
-          if (!ValidationService.isString(data[prop])) {
-            throw Error('Last name is not valid');
-          }
-          break;
-        case 'email':
-          if (ValidationService.isExist({ prop: data[prop] })) {
-            throw Error('Email is exist');
-          }
-          if (!ValidationService.isEmail(data[prop])) {
-            throw Error('Email must be gmail');
-          }
-          break;
-        case 'phoneNumber':
-          if (ValidationService.isExist({ prop: data[prop] })) {
-            throw Error('Phone number is exist');
-          }
-          if (!ValidationService.isPhoneNumber(data[prop])) {
-            throw Error('Phone number format +380XXXXXXXXX');
-          }
-          break
-        case 'password':
-          if (!ValidationService.isString(data[prop], 3)) {
-            throw Error('Password is not valid');
-          }
-          break;
-        default:
-          throw Error(`${prop} in the body of requests must be absent`);
-      }
-    }
-    next();
-  } catch (err) {
-    res.status(400).json({ error: true, message: err.message })
-  }
 }
 
 const updateUserValid = (req, res, next) => {
-    // TODO: Implement validatior for user entity during update
-
-    next();
+  // TODO: Implement validatior for user entity during update
+    try {
+        const dataUser = req.body;
+        if (Object.keys(dataUser).length === 0) {
+            throw Error('User entity must not be empty')
+        }
+        for (let key in dataUser) {
+            if (key === 'id' || !user.hasOwnProperty(key)) {
+                throw Error('User entity to update isn\'t valid')
+            }
+        }
+        const userValid = ValidationService.validateUser({ ...dataUser });
+        if (userValid) {
+            req.body = userValid;
+            next();
+        }
+    } catch (err) {
+        res.status(400).json({ error: true, message: err.message });
+    }
 }
 
 exports.createUserValid = createUserValid;

438a9b19f1653d6337df4aef881ba952a314618c
Dmitriy <dima.komlyk@gmail.com>
2022-06-13 21:55:21 +0300

    Created and added logic into get, create, update and delete routes for user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,6 +9,10 @@ const router = Router();
 router.get('/', async (req, res, next) => {
     try {
         const users = await UserService.getAll();
+        if (users.length === 0) {
+            res.status(404);
+            throw Error('Users are not found');
+        }
         res.data = users;
     } catch (err) {
         res.err = err;
@@ -17,15 +21,65 @@ router.get('/', async (req, res, next) => {
     }
 }, responseMiddleware);
 
+router.get('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const user = await UserService.search({ id });
+        if (!user) {
+            res.status(404);
+            throw Error('User is not found');
+        }
+        res.data = user;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+  }, responseMiddleware);
+
 router.post('/', createUserValid, async (req, res, next) => {
-  try {
-    const user = await UserService.create(req.body);
-    res.data = user;
-  } catch (err) {
-    res.err = err;
-  } finally {
-    next();
-  }
-}, responseMiddleware)
+    try {
+        const user = await UserService.create(req.body);
+        res.data = user;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
+router.put('/:id', updateUserValid, async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const user = await UserService.search({ id });
+        if (!user) {
+            res.status(404);
+            throw Error('User is not found');
+        }
+        const updatedUser = await UserService.update(id, req.body);
+        res.data = updatedUser;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
+
+router.delete('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const user = await UserService.search({ id });
+        if (!user) {
+            res.status(404);
+            throw Error('User is not found');
+        }
+        const deletedUser = await UserService.delete(id);
+        res.data = deletedUser;
+    } catch (err) {
+        res.err = err;
+    } finally {
+        next();
+    }
+}, responseMiddleware);
 
 module.exports = router;

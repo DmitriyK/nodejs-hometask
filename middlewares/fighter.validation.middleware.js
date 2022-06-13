e725ce0c454f66fe79c31db246c752eb1d9cd41e
Dmitriy <dima.komlyk@gmail.com>
2022-06-13 22:06:56 +0300

    Added logic into createFighterValid and updateFighterValid function

--- middlewares/fighter.validation.middleware.js
+++ middlewares/fighter.validation.middleware.js
@@ -1,13 +1,48 @@
 const { fighter } = require('../models/fighter');
+const ValidationService = require('../services/validationService');
 
 const createFighterValid = (req, res, next) => {
     // TODO: Implement validatior for fighter entity during creation
-    next();
+    try {
+        const dataFighter = req.body;
+        if (Object.keys(dataFighter).length === 0) {
+            throw Error('Fighter entity must not be empty')
+        }
+        for (let key in dataFighter) {
+            if (key === 'id' || !fighter.hasOwnProperty(key)) {
+                throw Error('Fighter entity to create isn\'t valid');
+            }
+        }
+        const fighterValid = ValidationService.validateFighter({ ...fighter, ...dataFighter });
+        if (fighterValid) {
+            req.body = fighterValid;
+            next();
+        };
+    } catch (err) {
+        res.status(400).json({ error: true, message: err.message })
+    }
 }
 
 const updateFighterValid = (req, res, next) => {
     // TODO: Implement validatior for fighter entity during update
-    next();
+    try {
+        const dataFighter = req.body;
+        if (Object.keys(dataFighter).length === 0) {
+            throw Error('Fighter entity must not be empty');
+        };
+        for (let key in dataFighter) {
+            if (key === 'id' || !fighter.hasOwnProperty(key)) {
+                throw Error('Fighter entity to create isn\'t valid');
+            }
+        };
+        const fighterValid = ValidationService.validateFighter({ ...dataFighter });
+        if (fighterValid) {
+            req.body = fighterValid;
+            next();
+        };
+    } catch (err) {
+        res.status(400).json({ error: true, message: err.message })
+    }
 }
 
 exports.createFighterValid = createFighterValid;

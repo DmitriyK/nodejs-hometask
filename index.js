93fcd812033ec4cf7b78cf27595f21bf80bd96dc
Dmitriy <dima.komlyk@gmail.com>
2022-06-13 21:32:57 +0300

    Added concole log

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ routes(app);
 app.use('/', express.static('./client/build'));
 
 const port = 3050;
-app.listen(port, () => {});
+app.listen(port, () => {
+    console.log(`Server run on port: ${port}`);
+});
 
 exports.app = app;

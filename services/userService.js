fb291cf3e667c86f7e6231e982d2af5fbef0f3f7
Dmitriy <dima.komlyk@gmail.com>
2022-06-13 22:03:30 +0300

    Added update and delete methods

--- services/userService.js
+++ services/userService.js
@@ -3,27 +3,43 @@ const { UserRepository } = require('../repositories/userRepository');
 class UserService {
   // TODO: Implement methods to work with user
   getAll() {
-    const item = UserRepository.getAll();
-    if(!item) {
-      return null;
-    }
-    return item;
+      const item = UserRepository.getAll();
+      if (!item) {
+          return null;
+      }
+      return item;
   }
 
   search(search) {
-    const item = UserRepository.getOne(search);
-    if(!item) {
-      return null;
-    }
-    return item;
+      const item = UserRepository.getOne(search);
+      if (!item) {
+          return null;
+      }
+      return item;
   }
 
   create(data) {
-    const item = UserRepository.create(data);
-    if(!item) {
-      return null;
-    }
-    return item;
+      const item = UserRepository.create(data);
+      if (!item) {
+          return null;
+      }
+      return item;
+  }
+
+  update(id, data) {
+      const item = UserRepository.update(id, data);
+      if (!item) {
+          return null;
+      }
+      return item;
+  }
+
+  delete(id) {
+      const item = UserRepository.delete(id);
+      if (!item) {
+          return null;
+      }
+      return item;
   }
 }
 

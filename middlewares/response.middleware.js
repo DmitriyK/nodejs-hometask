092f5fbbe4454f430c985bf04b7f16f74f1f2b75
Dmitriy <dima.komlyk@gmail.com>
2022-06-14 14:32:31 +0300

    Updated file

--- middlewares/response.middleware.js
+++ middlewares/response.middleware.js
@@ -1,13 +1,18 @@
 const responseMiddleware = (req, res, next) => {
   // TODO: Implement middleware that returns result of the query
-  if (res.err) {
-    res.json({ error: true, message: res.err.message });
+  switch(res.statusCode) {
+    case 200:
+      res.status(200).json(res.data);
+      break;
+    case 400:
+      res.status(400).json({ error: true, message: res.err.message });
+      break;
+    case 404:
+      res.status(404).json({ error: true, message: res.err.message });
+      break;
+    default:
+      break;
   }
-  
-  if (res.data){
-    res.status(200).json(res.data);
-  }
-
   next();
 }
 
